c8ca41f0bd19f48284804fa019150f5b38204b5c
BLANK <cyrus.simons@yahoo.com>
2018-09-15 12:59:28 -0400

    ye

--- js/scrubs.js
+++ js/scrubs.js
@@ -1,11 +1,3 @@
-let dropArea = document.getElementById('drop-area')
-
-  dropArea.addEventListener('dragenter', handlerFunction, false)
-  dropArea.addEventListener('dragleave', handlerFunction, false)
-  dropArea.addEventListener('dragover', handlerFunction, false)
-  dropArea.addEventListener('drop', handlerFunction, false)     
-
-
 function readURL(input) {
     if (input.files && input.files[0]) {
         var reader = new FileReader();
