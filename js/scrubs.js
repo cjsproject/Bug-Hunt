58cc37978ec363ad2fae4fb25007246411fba376
BLANK <cyrus.simons@yahoo.com>
2018-09-15 14:58:39 -0400

    includes file input

--- js/scrubs.js
+++ js/scrubs.js
@@ -11,3 +11,48 @@ function readURL(input) {
         reader.readAsDataURL(input.files[0]);
     }
 }
+
+function handleFileSelect(selector) {
+            var files = document.getElementById(selector).files; // FileList object
+
+            // The list will contain only one file.
+            for (var i = 0, f; f = files[i]; i++) {
+
+                // Only process image files.
+                if (!f.type.match('image.*')) {
+                    alert("Selected file must be an image file.");
+                    document.getElementById("trash").value = null;
+                    continue;
+                }
+
+                // Image must be <= 1 MB and should be about 1500px.
+                if (f.size > 1000000) {
+                    alert("Image must be less than 1 MB.");
+                    document.getElementById("trash").value = null;
+                    continue;
+                }
+
+
+                var reader = new FileReader();
+
+                // Capture the file information.
+                reader.onload = (function(theFile) {
+                    return function(e) {
+                        var fileOutput = document.getElementById('thumbnail');
+
+                        if (fileOutput.childElementCount > 0) {
+                            fileOutput.removeChild(fileOutput.lastChild);  // Remove the current pic, if it exists
+                        }
+
+                        // Render thumbnail.
+                        var span = document.createElement('span');
+                        span.innerHTML = ['<img class="thumb" src="', e.target.result,
+                                          '" title="', escape(theFile.name), '"/>'].join('');
+                        fileOutput.insertBefore(span, null);
+                    };
+                })(f);
+
+                // Read in the image file as a data URL.
+                reader.readAsDataURL(f);
+            }
+        }
